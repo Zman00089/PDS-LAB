2e730f3d91ada8c3146f4287b74397985057eb10
Zman00089 <153118724+Zman00089@users.noreply.github.com>
2024-03-29 00:12:12 +0530

    23CS01008_qsn2.c

--- 23CS01008_qsn2.c
+++ 23CS01008_qsn2.c
@@ -9,8 +9,8 @@ int main()
 {
     struct student
     {
-        char name[10];
-        char address[10];
+        char name[20];
+        char address[30];
         long int rollno;
         int age;
         double avg_marks;
@@ -21,21 +21,24 @@ int main()
     {
         printf("Enter name of student:");
         gets(students[i].name);
-        clearBuffer();
+
         printf("Enter address of student :");
         gets(students[i].address);
         clearBuffer();
+
         printf("Enter roll no. of student :");
         scanf("%ld", &students[i].rollno);
         printf("Enter age of student :");
         scanf("%d", &students[i].age);
         printf("Enter average marks of student :");
         scanf("%lf", &students[i].avg_marks);
+
+        clearBuffer();
     } 
 
     for(int i=0;i<6;i++)
     {
-        printf("\n%s\n%s\nRoll No.:%lld\tAge:%d\nAverage Marks:%lf",students[i].name,students[i].address,students[i].rollno,students[i].age,students[i].avg_marks);
+        printf("\nName:%s\nAddress:%s\nRoll No.:%lld\tAge:%d\nAverage Marks:%lf\n",students[i].name,students[i].address,students[i].rollno,students[i].age,students[i].avg_marks);
     }
     return 0;
 }
